cb57ab7ce9840d02d0600438e9a5111d91cc1d1d
Marcel Gerber <mgerber59@gmail.com>
2021-06-06 14:59:28 +0200

    fix

--- clientUtils/dayjs.ts
+++ clientUtils/dayjs.ts
@@ -1,6 +1,8 @@
-import dayjs, { Dayjs } from "dayjs/esm"
-import utc from "dayjs/esm/plugin/utc"
-import relativeTime from "dayjs/esm/plugin/relativeTime"
+// Imports dayjs and loads the plugins we need. Then exports it with the correct types.
+
+import dayjs, { Dayjs } from "dayjs"
+import utc from "dayjs/plugin/utc"
+import relativeTime from "dayjs/plugin/relativeTime"
 
 dayjs.extend(utc)
 dayjs.extend(relativeTime)
@@ -8,7 +10,7 @@ dayjs.extend(relativeTime)
 export default dayjs
 
 // We need these explicit plugin type imports _and exports_ to get the right Dayjs type down the line
-import type utcType from "dayjs/esm/plugin/utc"
-import type relativeTimeType from "dayjs/esm/plugin/relativeTime"
+import type utcType from "dayjs/plugin/utc"
+import type relativeTimeType from "dayjs/plugin/relativeTime"
 
 export type { Dayjs, utcType, relativeTimeType }
